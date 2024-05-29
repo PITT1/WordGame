d6cff4f030b029519f6ec03f1e4a3031ed2a8bd7
PITT1 <petroleo.plta.97@gmail.com>
2024-05-29 09:41:57 -0400

    arreglando los acertijos

--- src/utils/pistas.js
+++ src/utils/pistas.js
@@ -100,119 +100,119 @@ const pistas = [
     "Sinonimo de razon, entendimiento, discernimiento", // juicio
     "Principio moral que lleva a determinar que todos deben vivir honestamente", // justicia
     "Unidad de peso", // kilo
-    "Laboratorio científico", // laboratorio
+    "Lugar de experimentos cientificos", // laboratorio
     "Instrumento de escritura", // lápiz
     "Lactosa líquida", // leche
     "Tiene un monton de letras dentro", // libro
     "Limpiar superficies", // limpiar
     "Bonito aspecto", // lindo
-    "Línea recta", // línea
+    "Trasado en una pared, o papel", // línea
     "Illuminación natural", // luz
-    "Maestro docente", // maestro
+    "Persona que enseña a otros", // maestro
     "Matar ser vivo", // matar
-    "Rama de la medicina", // medicina
-    "Memoria mental", // memoria
-    "Pieza de mobiliario", // mesa
-    "Miedo al riesgo", // miedo
-    "Mil unidades", // mil
+    "Rama de la ciencia", // medicina
+    "Lo usamos para recordar y tambien esta dentro de las computadoras y celulares", // memoria
+    "Tabla con patas", // mesa
+    "Sentimiento de temor", // miedo
+    "1000", // mil
     "Intervalo de tiempo", // minuto
-    "Modelo de referencia", // modelo
-    "Criatura monstruosa", // monstruo
-    "Morir definitivamente", // morir
-    "Motor mecánico", // motor
-    "Nada absoluto", // nada
-    "Naturalmente", // natural
-    "Neblina atmosférica", // neblina
-    "Negocio comercial", // negocio
-    "Niño menor de edad", // niño
-    "Periodo nocturno", // noche
+    "Persona maniqui", // modelo
+    "Criatura muy temible", // monstruo
+    "Irse con mama coco", // morir
+    "Es el corazon de muchos vehiculos", // motor
+    "Sinonimo de vacio, inexistencia", // nada
+    "Sinonimo de innato, connatural, inherente, consustancial, congénito", // natural
+    "muy pequeñas gotas de agua en la atmosfera", // neblina
+    "Metodo de obtener dinero a cambio de productos o servicios", // negocio
+    "Menor de 12 años", // niño
+    "Cielo oscuro y estrellado", // noche
     "Narrativa literaria", // novela
-    "Objeto físico", // objeto
-    "Oculto secreto", // oculto
-    "Oficina de trabajo", // oficina
+    "Cosa fisica y tangible", // objeto
+    "Sinonimo de escondido", // oculto
+    "Lugar de trabajo", // oficina
     "Órganos visuales", // ojos
-    "Contrario opuesto", // opuesto
-    "Ordenar elementos", // orden
+    "Lo contrario", // opuesto
+    "Sinonimo de colocacion, disposicion, estructura, jerarquia", // orden
     "Metal precioso", // oro
-    "Otro diferente", // otro
-    "Padre biológico", // padre
-    "Palabra hablada", // palabra
-    "Pan de trigo", // pan
-    "Área verde", // parque
-    "Parte de algo", // parte
+    "Sinonimo de diferente, distinto", // otro
+    "hombre que engendra un hijo", // padre
+    "Unidad lingüística, dotada generalmente de significado", // palabra
+    "Alimento hecho de trigo", // pan
+    "Área verde para pasear", // parque
+    "Porcion de un todo", // parte
     "Transcurrir tiempo", // pasar
-    "Espacio abierto", // patio
-    "Pecado grave", // pecado
-    "Película cinematográfica", // película
-    "Perder algo", // perder
+    "Espacio abierto en una casa", // patio
+    "Desagradable para Dios", // pecado
+    "Largometraje", // película
+    "Dejar de tener, o no hallar, aquello que poseía", // perder
     "Individuo humano", // persona
-    "Piensa reflexionar", // piensa
-    "Moneda plateada", // plata
-    "Plantar semillas", // plantar
-    "Plaza pública", // plaza
-    "Poema lírico", // poema
-    "Política gubernamental", // política
+    "Estas pensando", // piensa
+    "Material de la tabla periodica", // plata
+    "Accion de sembrar una planta", // plantar
+    "Zona publica para caminar", // plaza
+    "Obra escrita en verso", // poema
+    "Toma de decisiones en grupo, u otras formas de relaciones de poder entre personas", // política
     "Colocar algo", // poner
     "Población urbana", // pueblo
-    "Puerto marítimo", // puerto
-    "Punto cardinal", // punto
-    "Querer intensamente", // querer
-    "Velocidad alta", // rápido
-    "Rayo eléctrico", // rayo
-    "Recibir algo", // recibir
-    "Recordar eventos", // recordar
-    "Refrescar mente", // refrescar
-    "Regresar lugar", // regresar
-    "Religión creencias", // religión
-    "Remedio curativo", // remedio
+    "Lugar en la costa o en las orillas de un río", // puerto
+    "Forman parte de i y j", // punto
+    "Desear o apetecer", // querer
+    "Rayo McQueen", // rápido
+    "Línea de luz que procede de un cuerpo luminoso", // rayo
+    "Tomar lo que le dan o le envían", // recibir
+    "Pasar a tener en la mente algo del pasado", // recordar
+    "Atemperar, moderar o disminuir el calor de algo", // refrescar
+    "volver a un lugar que ya visitaste", // regresar
+    "Creencia basada en dogmas", // religión
+    "Liquido curativo", // remedio
     "Arrepentirse acto", // repentir
-    "Respirar aire", // respirar
+    "Sinonimo de inhalar", // respirar
     "Característica facial", // rostro
     "Sabiduría adquirida", // saber
-    "Salir fuera", // salir
-    "Salud bienestar", // salud
-    "Sandwich relleno", // sandwich
-    "Secreto privado", // secreto
-    "Sentimiento emocional", // sentimiento
-    "Siempre continuamente", // siempre
-    "Silencio total", // silencio
-    "Simplemente solo", // simplemente
-    "Sin presencia", // sin
+    "Pasar de dentro afuera", // salir
+    "Estado en que el ser orgánico ejerce normalmente todas sus funciones", // salud
+    "Pan con jamon y queso", // sandwich
+    "Cosa que cuidadosamente se tiene reservada y oculta", // secreto
+    "Emociones internas", // sentimiento
+    "En todo o en cualquier tiempo", // siempre
+    "Ausencia de sonido", // silencio
+    "Con simpleza o sencillez", // simplemente
+    "Carencia o falta de algo", // sin
     "Lugar específico", // sitio
-    "Soñar imágenes", // soñar
-    "Sonrisa feliz", // sonrisa
+    "Lo hacemos cuando dormimos", // soñar
+    "Exprecion facial de alegria", // sonrisa
     "Descanso nocturno", // sueño
     "Superficie firme", // suelo
-    "Superior a otros", // superior
+    "Que está más alta y en lugar preeminente respecto de otra", // superior
     "Emerger aparentemente", // surgir
-    "Tarea asignada", // tarea
-    "Temprano madrugada", // temprano
-    "Tiempo transcurrido", // tiempo
-    "Tinta escribible", // tinta
-    "ToCAR instrumento", // tocar
+    "Deberes para hacer", // tarea
+    "Primeras horas de la mañana", // temprano
+    "Nos hace envejecer", // tiempo
+    "Liquido oscuro usado para escribir", // tinta
+    "Accion que tiene que ver con el tacto", // tocar
     "Fruta roja", // tomate
-    "Tonos musicales", // tono
-    "Trabajo manual", // trabajo
-    "Calmado tranquilo", // tranquilo
-    "Transmitir información", // transmitir
-    "Trauma dañino", // trauma
+    "Cualidad de los sonidos", // tono
+    "Sinonimo de laburo", // trabajo
+    "Sinonimo de calmado", // tranquilo
+    "Enviar informacion o contagiar enfermedad", // transmitir
+    "Choque emocional que produce un daño duradero en el inconsciente", // trauma
     "Grupo étnico", // tribu
     "Actividad turística", // turismo
-    "Último final", // último
-    "Unirse grupo", // unirse
-    "Utilidad práctica", // utilidad
-    "Vaca mamífero", // vaca
-    "Valiente arrojado", // valiente
-    "Valor monetario", // valor
-    "Variedad diversa", // variedad
+    "Está al final de una línea, de una serie o de una sucesión", // último
+    "Antonimo de separarse", // unirse
+    "Cualidad util", // utilidad
+    "Mooo", // vaca
+    "Antonimo de cobarde", // valiente
+    "Grado de utilidad o aptitud de las cosas para satisfacer las necesidades o proporcionar bienestar o deleite", // valor
+    "Cualidad de vario", // variedad
     "Móvil rápido", // veloz
-    "Ventana abierta", // ventana
-    "Verdad real", // verdad
-    "Vida existencia", // vida
+    "Gran hueco en una pared que nunca se tapa aproposito", // ventana
+    "Antonimo de mentira", // verdad
+    "Todos los seres animados lo tienen", // vida
     "Instrumento musical", // violín
-    "Volar en el aire", // volar
-    "Voluntario ofrecimiento", // voluntario
-    "Vuelta retorno", // vuelta
+    "Pajaros y aviones", // volar
+    "Que se hace por espontánea voluntad y no por obligación o deber", // voluntario
+    "Movimiento de una cosa alrededor de un punto", // vuelta
     "Alimento fermentado", // yogur
     "zapato pero en femenino", // zapatilla
 ];
